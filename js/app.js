697d6b00926ea1e3dbe6986f0a14f6a8f15889ab
thiagofrivs <thiagofrivs01@gmail.com>
2023-01-18 15:48:26 -0300

    css terminado y array de productos terminado, falta toda la estructura del JavaScript

---
+++ js/app.js
@@ -0,0 +1,213 @@
+//PRODUCTOS
+
+const productos=[
+    //PROCESADORES
+    {
+        id: "procesador-01",
+        titulo:"Procesador 01",
+        imagen:"./img/procesadores/01.jpg",
+        categoria:{
+            nombre:"Procesadores",
+            id:"procesadores"
+        },
+        precio:30000
+    },
+    {
+        id: "procesador-02",
+        titulo:"Procesador 02",
+        imagen:"./img/procesadores/01.jpg",
+        categoria:{
+            nombre:"Procesadores",
+            id:"procesadores"
+        },
+        precio:35000
+    },
+    {
+        id: "procesador-03",
+        titulo:"Procesador 03",
+        imagen:"./img/procesadores/03.jpg",
+        categoria:{
+            nombre:"Procesadores",
+            id:"procesadores"
+        },
+        precio:40000
+    },
+    {
+        id: "procesador-04",
+        titulo:"Procesador 04",
+        imagen:"./img/procesadores/04.jpg",
+        categoria:{
+            nombre:"Procesadores",
+            id:"procesadores"
+        },
+        precio:45000
+    },
+    {
+        id: "procesador-05",
+        titulo:"Procesador 05",
+        imagen:"./img/procesadores/05.jpg",
+        categoria:{
+            nombre:"Procesadores",
+            id:"procesadores"
+        },
+        precio:45000
+    },
+
+    //TECLADOS
+
+    {
+        id: "teclado-01",
+        titulo:"teclado 01",
+        imagen:"./img/teclados/01.jpg",
+        categoria:{
+            nombre:"teclados",
+            id:"teclados"
+        },
+        precio:5000
+    },
+    {
+        id: "teclado-02",
+        titulo:"teclado 02",
+        imagen:"./img/teclados/02.jpg",
+        categoria:{
+            nombre:"teclados",
+            id:"teclados"
+        },
+        precio:6000
+    },
+    {
+        id: "teclado-03",
+        titulo:"teclado 03",
+        imagen:"./img/teclados/03.jpg",
+        categoria:{
+            nombre:"teclados",
+            id:"teclados"
+        },
+        precio:6000
+    },
+    {
+        id: "teclado-04",
+        titulo:"teclado 04",
+        imagen:"./img/teclados/04.jpg",
+        categoria:{
+            nombre:"teclados",
+            id:"teclados"
+        },
+        precio:7000
+    },
+    {
+        id: "teclado-05",
+        titulo:"teclado 05",
+        imagen:"./img/teclados/05.jpg",
+        categoria:{
+            nombre:"teclados",
+            id:"teclados"
+        },
+        precio:8000
+    },
+
+    //MONITORES
+    {
+        id: "monitor-01",
+        titulo:"monitor 01",
+        imagen:"./img/monitores/01.jpg",
+        categoria:{
+            nombre:"monitores",
+            id:"monitores"
+        },
+        precio:30000
+    },
+    {
+        id: "monitor-02",
+        titulo:"monitor 02",
+        imagen:"./img/monitores/02.jpg",
+        categoria:{
+            nombre:"monitores",
+            id:"monitores"
+        },
+        precio:40000
+    },
+    {
+        id: "monitor-03",
+        titulo:"monitor 03",
+        imagen:"./img/monitores/03.jpg",
+        categoria:{
+            nombre:"monitores",
+            id:"monitores"
+        },
+        precio:35000
+    },
+    {
+        id: "monitor-04",
+        titulo:"monitor 04",
+        imagen:"./img/monitores/04.jpg",
+        categoria:{
+            nombre:"monitores",
+            id:"monitores"
+        },
+        precio:45000
+    },
+    {
+        id: "monitor-05",
+        titulo:"monitor 05",
+        imagen:"./img/monitores/05.jpg",
+        categoria:{
+            nombre:"monitores",
+            id:"monitores"
+        },
+        precio:50000
+    },
+
+    //MEMORIAS
+
+    {
+        id: "memoria-01",
+        titulo:"memoria 01",
+        imagen:"./img/memorias/01.jpg",
+        categoria:{
+            nombre:"memorias",
+            id:"memorias"
+        },
+        precio:7000
+    },
+    {
+        id: "memoria-02",
+        titulo:"memoria 02",
+        imagen:"./img/memorias/02.jpg",
+        categoria:{
+            nombre:"memorias",
+            id:"memorias"
+        },
+        precio:8000
+    },
+    {
+        id: "memoria-03",
+        titulo:"memoria 03",
+        imagen:"./img/memorias/03.jpg",
+        categoria:{
+            nombre:"memorias",
+            id:"memorias"
+        },
+        precio:8000
+    },
+    {
+        id: "memoria-04",
+        titulo:"memoria 04",
+        imagen:"./img/memorias/04.jpg",
+        categoria:{
+            nombre:"memorias",
+            id:"memorias"
+        },
+        precio:8000
+    },
+    {
+        id: "memoria-05",
+        titulo:"memoria 05",
+        imagen:"./img/memorias/05.jpg",
+        categoria:{
+            nombre:"memorias",
+            id:"memorias"
+        },
+        precio:10000
+    },
+]
