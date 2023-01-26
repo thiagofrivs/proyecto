4873160d2bacc87e0a9df6634a920e643f6a9ed4
thiagofrivs <thiagofrivs01@gmail.com>
2023-01-26 14:53:32 -0300

    Proyecto terminado. Creo

--- js/app.js
+++ js/app.js
@@ -227,7 +227,7 @@ function cargarProductos(productosElegidos){
                         <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
                         <div class="producto-detalles">
                             <h3 class="producto-titulo">${producto.titulo}</h3>
-                            <p class="producto-precio">${producto.precio}</p>
+                            <p class="producto-precio">$${producto.precio}</p>
                             <button class="producto-agregar" id="${producto.id}">Agregar</button>
                         </div>`
         contenedorProductos.append(div)
